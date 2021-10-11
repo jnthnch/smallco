ef2158ab7d46d826a8dd06d3786afab7674a25d8
jnthnch <jchu127@gmail.com>
2021-10-11 14:26:41 -0700

    Update to add alert to browser when status code is 401

--- public/js/login.js
+++ public/js/login.js
@@ -1,8 +1,6 @@
 document.getElementById('loginButton').onclick = function () {
     const username = document.getElementById('username').value
     const password = document.getElementById('pass').value
-    console.log('username: ', username)
-    console.log('password: ', password)
 
     let body = {
         username: username,
@@ -10,28 +8,26 @@ document.getElementById('loginButton').onclick = function () {
     }
 
     async function postLogin(url = '', data = {}) {
-        // Default options are marked with *
 
         const response = await fetch(url, {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            method: 'POST', 
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': '*/*'
-
-          },
-          body: JSON.stringify(data) // body data type must match "Content-Type" header
+            },
+            body: JSON.stringify(data)
         });
 
-        return response// parses JSON response into native JavaScript objects
+        return response
     }
       
     postLogin('http://localhost:3000/login', body)
         .then(data => {
-            window.location.href = data.url;
-            console.log('[response after POSTing to /login]', data)
-        })
-        .catch(error => {
-            console.log('[error]: ', error)
+            if (data.status === 401) {
+                alert('username or password is incorrect')
+            } else {
+                window.location.href = data.url;
+            }
         })
     
 }
