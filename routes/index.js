2c1bfec46dffec22fd155d82b7a8260ca94fd606
jnthnch <jchu127@gmail.com>
2021-10-11 14:25:05 -0700

    Reformatted routes

--- routes/index.js
+++ routes/index.js
@@ -10,102 +10,108 @@
 // route them to homepage after registration
 
 // after signout, clear out cookie and redirect to login page
-
-
 const express = require('express');
 const path = require('path');
 const db = require('../databaseConnection');
 const router = express.Router();
+const axios = require('axios');
 
 const jwt = require('jsonwebtoken')
 
-const SECRET_FOR_JWT = 'SECRET FOR JWT'
+const SECRET_FOR_JWT = process.env.SECRET_FOR_JWT
 
 router.use(function (req, res, next) {
     next()
 })
 
-router.get('/', async (req, res) => {
-    if (req.cookies) {
-        // console.log('[req.cookies]:', req.cookies)
-        if (!req.cookies.jwt) {
-            // redirect to login
-            return res.sendFile(path.resolve(__dirname, '../public/login.html'))
-        } else {
-                // validate jwt
-            try {
-                const tokenFromClient = req.cookies.jwt
-                const decoded = await jwt.verify(tokenFromClient, SECRET_FOR_JWT)
-
-                return res.status(200).sendFile(path.resolve(__dirname, '../public/welcome.html'));
-
-            } catch {
-                // console.log('jwt is bad')
+router.route('/')
+    .get(async function (req, res) {
+        if (req.cookies) {
+            if (!req.cookies.jwt) {
+                // redirect to login
                 return res.sendFile(path.resolve(__dirname, '../public/login.html'))
-            }
-        }
-    }
-
-})
-
-router.get('/login', async (req, res, next) => {
-    // return res.sendFile(path.resolve(__dirname, '../public/login.html'))
-    try {
-        const tokenFromClient = req.cookies.jwt
-        const decoded = await jwt.verify(tokenFromClient, SECRET_FOR_JWT)
-
-        // return res.status(200).sendFile(path.resolve(__dirname, '../public/welcome.html'));
-        return res.status(200).redirect('/welcome')
-    } catch {
-
-        // console.log('jwt is bad')
-        return res.sendFile(path.resolve(__dirname, '../public/login.html'))
-    }
-})
-
-//          login: check if user and password are valid. 
-            // If valid, create cookie and send back to user, redirect to /welcome
-            // if invalid, redirect back to /login
-
-router.post('/login', (req, res, next) => {
-    db.query(
-        "SELECT * FROM users WHERE username=$1 AND password=$2 LIMIT 1",
-        [req.body.username, req.body.password]
-    )
-        .then(foundUser => {
-            let user = foundUser.rows[0]
-            
-            console.log("[foundUser]: ", user)
-            if (user === undefined) {
-                return res.status(401).json({ message: 'username or password is wrong'})
-                // create token
             } else {
-                let tokenToClient = jwt.sign(user, SECRET_FOR_JWT, { expiresIn: 60 * 400 })
-                console.log('[token to client]:', tokenToClient)
-                res.cookie('jwt', tokenToClient)
-                res.cookie('username', user.username)
-                
-                // res.send({ redirect_path: "/welcome" });
-                return res.redirect('/welcome')
+                try {
+                    const tokenFromClient = req.cookies.jwt
+                    const decoded = await jwt.verify(tokenFromClient, SECRET_FOR_JWT)
 
-                // return res.json({ jwt: tokenToClient})
+                    return res.status(200).sendFile(path.resolve(__dirname, '../public/welcome.html'));
+                } catch {
+                    return res.sendFile(path.resolve(__dirname, '../public/login.html'))
+                }
             }
-        })
-        .catch(e => {
-            console.log('error querying username: ', e)
-            return res.status(500).json({ message: 'error with internal query'})
-        })
-})
+        } else {
+            return res.sendFile(path.resolve(__dirname, '../public/login.html'))
+        }
+    })
 
-router.route("/logout")
+router.route('/login')
+    .get(async function(req, res, next) {
+        try {
+            const tokenFromClient = req.cookies.jwt
+            await jwt.verify(tokenFromClient, SECRET_FOR_JWT)
+
+            return res.status(200).redirect('/welcome')
+        } catch {
+
+            return res.sendFile(path.resolve(__dirname, '../public/login.html'))
+        }
+    })
+    .post(function (req, res, next) {
+        let username = req.body.username;
+        let password = req.body.password;
+        let url = `http://localhost:5000/users?username=${username}`;
+
+        axios.get(url)
+            .then(response => {
+                let user = response.data[0];
+                let dbPassword = user.password;
+
+                if (password === dbPassword) {
+                    console.log('password === dbPassword!!!');
+                } else {
+                    return res.status(401).json({ message: 'username or password is wrong' })
+                    // return res.redirect('/error');
+                }
+
+                let safeUser = {
+                    id: user.id,
+                    username: user.username
+                }
+
+                if (user === undefined) {
+                    return res.status(401).json({ message: 'username or password is wrong' })
+                    // create token
+                } else {
+                    let tokenToClient = jwt.sign(safeUser, SECRET_FOR_JWT, { expiresIn: 60 * 400 })
+                    console.log('[token to client]:', tokenToClient)
+                    res.cookie('jwt', tokenToClient)
+                    res.cookie('username', user.username)
+
+                    // res.send({ redirect_path: "/welcome" });
+                    return res.redirect('/welcome')
+                    // return res.json({ jwt: tokenToClient})
+                }
+            })
+            .catch(e => {
+                console.log('error querying username: ', e)
+                return res.status(401).json({ message: 'username or password is wrong' })
+            })
+    })
+
+router.route('/logout')
     .post(function (req, res, next) {
         console.log('/logout HITTTT')
         res.clearCookie('jwt').redirect('/login');
     })
 
+router.route('/error')
+    .get(function (req, res, next) {
+        return res.status(403).sendFile(path.resolve(__dirname, '../public/error.html'));
+    })
+
 router.route('/welcome')
     .get(async function (req, res, next) {
-
         if (!req.cookies.jwt) {
             // redirect to login
             return res.redirect('/login')
@@ -132,6 +138,11 @@ router.route('/welcome')
             message: "Created user",
             users: users
         })
+    })
+
+router.route('/picks')
+    .post(async (req, res, next) => {
+        console.log('[req]', req.body)
     })
 
 module.exports = router
