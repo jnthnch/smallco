c1d88fa3aaa22952a734da5ff1aa035d13581920
jnthnch <jchu127@gmail.com>
2021-10-18 15:20:37 -0700

    Updated welcome pages

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -1,4 +1,46 @@
-async function getUsername() {
+function init() {
+    getUsername();
+    fetchGames();
+
+}
+
+function fetchGames() {
+    axios.get('http://localhost:5000/games')
+        .then(function (response) {
+            let gamesArray = response.data;
+            for (let i = 0; i < gamesArray.length; i++) {
+                addGame(gamesArray[i])
+            }
+        })
+        .catch(function (error) {
+            console.log('[error]: ', error)
+        })
+}
+
+function addGame(game) {
+    const gamePicksContainer = document.getElementsByClassName('game_picks_container')
+    const gamePick = document.createElement('game-pick')
+
+    gamePick.addEventListener('jonEvent', function (event) {
+        console.log('it works!!!')
+        console.log('event', event.detail)
+    })
+
+    const { homeTeam, awayTeam, homeSpread, awaySpread } = game;
+
+    console.log('[game]', game)
+    // console.log('gamePicksContainer', gamePicksContainer)
+
+    gamePick.setAttribute('team-home-name', homeTeam)
+    gamePick.setAttribute('team-away-name', awayTeam)
+    gamePick.setAttribute('team-home-spread', homeSpread)
+    gamePick.setAttribute('team-away-spread', awaySpread)
+
+    gamePicksContainer[0].appendChild(gamePick)
+    // console.log('game: ', game)
+}
+
+function getUsername() {
     let username = document.cookie.username;
     let cookieInArray = document.cookie.split('; ');
 
@@ -33,18 +75,11 @@ document.getElementById('logout_button').onclick = async function () {
         })
 }
 
-axios.get('http://localhost:5000/games')
-    .then(function (response) {
-        console.log('games response:', response.data)
-    })
-    .catch(function (error) {
-        console.log('[error]: ', error)
-    })
 
-function handleSubmit() {
-    let selection = document.getElementById('game-1-selection')
-    console.log('selection: ', selection.value)
-    console.log('handle submit clicked')
-}
+// function handleSubmit() {
+//     let selection = document.getElementById('game-1')
+//     console.log('selection: ', selection)
+//     console.log('handle submit clicked')
+// }
 
-getUsername();
+init();
